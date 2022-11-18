e7cfbe0e5682cccf7edcac5b3e9caf631f09c15c
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-11-18 03:28:52 -0800

    added bootstrap styles

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -1,24 +1,40 @@
 import React from 'react'
+import './footer.css'
 import Col from 'react-bootstrap/esm/Col'
 import Row from 'react-bootstrap/esm/Row'
+import {AiFillEye} from 'react-icons/ai'
+import {FaHandshake} from 'react-icons/fa'
+import {FiHelpCircle} from 'react-icons/fi'
 
 const footer = () => {
     return (
         <>
-            <Row xs={1} sm={2} md={3}>
-                <Col>  <h1 className="footer-logo">
+        <hr/>
+            <Row xs={1} sm={2} md={3} mt={5} className="center">
+                <Col  xs={{ span: 6, offset: 3 }}>  
                     <span>FOOD.</span>
                     <span>VAULT</span>
-                </h1></Col>
-                <Col>
-                <div>
-                    <a href="">about</a>
-                    <a href="">contact</a>
-                    <a href="">help</a>
-                </div>
                 </Col>
-                <Col>medium</Col>
+                <Row xs={1} md={3}>
+                <Col  xs={{ span: 6, offset: 5 }}>
+
+                    <a href="" > <AiFillEye/> about</a>
+                </Col>
+                <Col  xs={{ span: 6, offset: 5 }}>
+                    <a href=""> <FaHandshake/> contact</a>
+                </Col>
+                <Col  xs={{ span: 6, offset: 5 }}>
+                    <a href=""> <FiHelpCircle/> help</a>
+                </Col>
+                </Row>
+                <Col  xs={{ span: 6, offset: 4 }}>medium</Col>
             </Row>
+            <hr className='hr' />
+                <div className='copyright'>
+                    <p>
+                        Copyright © 2022 • Team Drive
+                    </p>
+                </div>
         </>
     )
 }
