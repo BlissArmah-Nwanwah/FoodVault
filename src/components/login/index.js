86dd8b5896d4745675596af7588551a27aed2b91
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-12-21 10:29:02 +0000

    additional styles

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -30,12 +30,13 @@ const Login = () => {
     }
   }
   return (
-      <div className='forms' >
+      <div className=' forms' >
         <div className="img-container">
         <img src={Image} alt="" />
         </div>
-        
+
       <form >
+        
   <div className="form-outline mb-4">
     <input type="email" id="form1Example1" className="form-control inp" onChange={(e)=> setEmail(e.target.value)}/>
     <label className="form-label" for="form1Example1">Email address</label>
@@ -44,6 +45,8 @@ const Login = () => {
   <div className="form-outline mb-4">
     <input type="password" id="form1Example2" className="form-control inp" onChange={(e)=> setPassword(e.target.value)}  />
     <label className="form-label" for="form1Example2">Password</label>
+
+    
   </div>
 
   <div className="mb-2 login-footer">
